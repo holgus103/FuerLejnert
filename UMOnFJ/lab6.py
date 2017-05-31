2df5aaeedc2b5ff5828dbc3dd6561a6f41acc52a
holgus103 <holgus103@gmail.com>
2017-05-31 17:34:22 +0200

    Failure - errors with custom kernel functions

--- UMOnFJ/lab6.py
+++ UMOnFJ/lab6.py
@@ -1,39 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import numpy as np
 from preprocessing import getData
 from sklearn.svm import SVR
+import kernelFunctions as kf
 
 #import matplotlib.pyplot as plt
 #from matplotlib import cm
 #from mpl_toolkits.mplot3d import Axes3D
 
-sciezka = '../lab6/'
+sciezka = './dane/'
 print('Uzywam sciezki: ' + sciezka)
-xlearn, rlearn = getData(sciezka + 'train7.csv')
-xtest, rtest = getData(sciezka + 'test7.csv')
+sets = [['train6.csv', 'test6.csv'], ['train7.csv', 'test7.csv']]
 
-svr_rbf = SVR(kernel='rbf', C=1, gamma=0.1)
-svr_lin = SVR(kernel='linear', C=1)
-svr_poly = SVR(kernel='poly', C=1, degree=2)
+# each entry is a list of C, gamma and degree
+configs = [[1, 0.1, 2], [5, 0.2, 4], [20, 0.4, 8]]
+for set_num in range(0, len(sets)):
+    xlearn, rlearn = getData(sciezka + sets[set_num][0])
+    xtest, rtest = getData(sciezka + sets[set_num][1])
+    print(sets[set_num])
+    for c_num in range(0, len(configs)):
+        svr_rbf = SVR(kernel='rbf', C=configs[c_num][0], gamma=configs[c_num][1])
+        #svr_lin = svr(kernel='linear', c=configs[c_num][0])
+        #svr_poly = svr(kernel='poly', c=configs[c_num][0], degree=configs[c_num][2])
+        r_rbf = svr_rbf.fit(xlearn, rlearn).predict(xtest)
+        print(xlearn)
+        print(xtest)
+        s_laplacean = SVR(kernel ='precomputed').fit(kf.laplacean(xlearn, xlearn, configs[c_num][1]), rlearn)
+        r_laplacean = s_laplacean.predict(kf.laplacean(xtest, xtest, configs[c_num][1]))
+        r_sinc = SVR(kernel ='precomputed').fit(kf.sinc(xlearn, xlearn, configs[c_num][0]), rlearn).predict(kf.sinc(xtest, xtest, configs[c_num][0]))
+        r_quadratic = SVR(kernel ='precomputed').fit(kf.quadratic(xlearn, xlearn, configs[c_num][0]), rlearn).predict(kf.quadratic(xtest, xtest, configs[c_num][0]))
+        r_multiquadric = SVR(kernel ='precomputed').fit(kf.multiquadric(xlearn, xlearn, configs[c_num][0]), rlearn).predict(kf.multiquadric(xtest, xtest, configs[c_num][0]))
+        r_rbf = svr_rbf.fit(xlearn, rlearn).predict(xtest)
+        r_lin = svr_lin.fit(xlearn, rlearn).predict(xtest)
+        r_poly = svr_poly.fit(xlearn, rlearn).predict(xtest)
 
-r_rbf = svr_rbf.fit(xlearn, rlearn).predict(xtest)
-r_lin = svr_lin.fit(xlearn, rlearn).predict(xtest)
-r_poly = svr_poly.fit(xlearn, rlearn).predict(xtest)
+        print('Tu liczymy błąd aproksymacji:')
+        blad_rbf=0;
+        for i in range (0,len(r_rbf)-1):
+            blad_rbf=blad_rbf+(r_rbf[i]-rtest[i])**2
+        print('Błąd regresji dla funkcji rbf: ', blad_rbf)
 
-print('Tu liczymy błąd aproksymacji:')
-blad_rbf=0;
-for i in range (0,len(r_rbf)-1):
-    blad_rbf=blad_rbf+(r_rbf[i]-rtest[i])**2  
-print('Błąd regresji dla funkcji rbf: ', blad_rbf)
+        blad_lin=0;
+        for i in range (0,len(r_lin)-1):
+            blad_lin=blad_lin+(r_lin[i]-rtest[i])**2
+        print('Błąd regresji dla funkcji liniowej: ', blad_lin)
 
-blad_lin=0;
-for i in range (0,len(r_lin)-1):
-    blad_lin=blad_lin+(r_lin[i]-rtest[i])**2  
-print('Błąd regresji dla funkcji liniowej: ', blad_lin)
+        blad_poly=0;
+        for i in range (0,len(r_poly)-1):
+            blad_poly=blad_poly+(r_poly[i]-rtest[i])**2
+        print('Błąd regresji dla funkcji wielomianowej: ', blad_poly)
 
-blad_poly=0;
-for i in range (0,len(r_poly)-1):
-    blad_poly=blad_poly+(r_poly[i]-rtest[i])**2  
-print('Błąd regresji dla funkcji wielomianowej: ', blad_poly)
+        print('Tu liczymy błąd aproksymacji:')
+        blad_rbf=0;
+        for i in range (0,len(r_laplacean)-1):
+            blad_rbf=blad_rbf+(r_laplacean[i]-rtest[i])**2
+        print('Błąd regresji dla funkcji laplacean: ', blad_rbf)
+
+        blad_lin=0;
+        for i in range (0,len(r_sinc)-1):
+            blad_lin=blad_lin+(r_sinc[i]-rtest[i])**2
+        print('Błąd regresji dla funkcji sinc: ', blad_lin)
+
+        blad_poly=0;
+        for i in range (0,len(r_quadratic)-1):
+            blad_poly=blad_poly+(r_quadratic[i]-rtest[i])**2
+        print('Błąd regresji dla funkcji quadratic: ', blad_poly)
+
+        blad_poly=0;
+        for i in range (0,len(r_multiquadric)-1):
+            blad_poly=blad_poly+(r_multiquadric[i]-rtest[i])**2
+        print('Błąd regresji dla funkcji multiquadric: ', blad_poly)
+
 
 
 #fig=plt.figure()
@@ -55,5 +93,3 @@ print('Błąd regresji dla funkcji wielomianowej: ', blad_poly)
 ##fig.colorbar(surf3, shrink=0.5, aspect=5)
 ##plt.title('Support Vector Regression')
 ##plt.show()
-
-
